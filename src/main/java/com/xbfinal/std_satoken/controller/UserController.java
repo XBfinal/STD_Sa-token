1925fb3ccae98017306fd6287848081339b90d09
xbfinal <358863605@qq.com>
2023-04-08 16:37:25 +0800

    sa-token测试

---
+++ src/main/java/com/xbfinal/std_satoken/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.xbfinal.std_satoken.controller;
+
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title: UserController
+ * @Author 笑霸fianl
+ * @Package com.xbfinal.std_satoken.controller
+ * @Date 2023/4/8 16:32
+ * @描述:
+ */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+}
