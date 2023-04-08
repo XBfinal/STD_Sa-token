b2160e1a83478c267a95bfd1ec9693fcde01baf1
xbfinal <358863605@qq.com>
2023-04-08 16:25:30 +0800

    搭建仓库

---
+++ src/main/java/com/xbfinal/std_satoken/StdSaTokenApplication.java
@@ -0,0 +1,13 @@
+package com.xbfinal.std_satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdSaTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdSaTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xbfinal/std_satoken/StdSaTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbfinal.std_satoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdSaTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
